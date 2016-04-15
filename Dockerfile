989b5e9a19bba794ee8ddd3ed5a49cc1608b3440
Quinten De Swaef <quintendeswaef@gmail.com>
2016-04-15 13:34:55 +0200

    installing invoiceninja now happens in the app-entrypoint

--- Dockerfile
+++ Dockerfile
@@ -18,23 +18,6 @@ RUN apt-get update \
 #####
 RUN curl -sS https://getcomposer.org/installer | php -- --install-dir=/usr/local/bin --filename=composer
 
-VOLUME /var/www/app
-
-#####
-# DOWNLOAD AND INSTALL INVOICE NINJA
-#####
-
-ENV INVOICENINJA_VERSION 2.5.1.3
-#ENV INVOICENINJA_SHA1 3e9b63c1681b6923dc1a24399411c1abde6ef5ea
-
-RUN curl -o invoiceninja.tar.gz -SL https://github.com/hillelcoren/invoice-ninja/archive/v${INVOICENINJA_VERSION}.tar.gz \
-#    && echo "$INVOICENINJA_SHA1 *invoiceninja.tar.gz" | sha1sum -c - \
-    && tar -xzf invoiceninja.tar.gz -C /var/www/ \
-    && rm invoiceninja.tar.gz \
-    && mv /var/www/invoiceninja-${INVOICENINJA_VERSION}/* /var/www/app/. \
-    && chown -R www-data:www-data /var/www/app \
-    && composer install --working-dir /var/www/app -o --no-dev --no-interaction  --prefer-source
-
 
 ######
 # DEFAULT ENV
@@ -46,7 +29,7 @@ ENV LOG errorlog
 ENV APP_DEBUG 0
 
 
-
+VOLUME /var/www/app
 WORKDIR /var/www/app
 
 EXPOSE 80
--- app-entrypoint.sh
+++ app-entrypoint.sh
@@ -1,10 +1,32 @@
 #!/bin/bash
 set -e
 
-echo "adding username and password to the .env file"
-echo "DB_USERNAME=$DB_USERNAME" >> .env
-echo "DB_PASSWORD=$DB_PASSWORD" >> .env
+#####
+# DOWNLOAD AND INSTALL INVOICE NINJA
+#####
 
-chown www-data .env
+ENV INVOICENINJA_VERSION 2.5.1.3
+#ENV INVOICENINJA_SHA1 3e9b63c1681b6923dc1a24399411c1abde6ef5ea
+
+if [ -f /.invoiceninja-installed ]; then
+	echo "installing invoiceninja for the first time"
+	RUN curl -o invoiceninja.tar.gz -SL https://github.com/hillelcoren/invoice-ninja/archive/v${INVOICENINJA_VERSION}.tar.gz \
+	#    && echo "$INVOICENINJA_SHA1 *invoiceninja.tar.gz" | sha1sum -c - \
+	    && tar -xzf invoiceninja.tar.gz -C /var/www/ \
+	    && rm invoiceninja.tar.gz \
+	    && mv /var/www/invoiceninja-${INVOICENINJA_VERSION}/* /var/www/app/. \
+	    && chown -R www-data:www-data /var/www/app \
+	    && composer install --working-dir /var/www/app -o --no-dev --no-interaction  --prefer-source
+
+			echo "gratz! invoiceninja installed!"
+
+			echo "adding username and password to the .env file"
+			echo "DB_USERNAME=$DB_USERNAME" >> .env
+			echo "DB_PASSWORD=$DB_PASSWORD" >> .env
+
+			chown www-data .env
+fi
+
+touch /.invoiceninja-installed
 
 exec "$@"
