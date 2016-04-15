ac14e2292ae48735b251a662bb7b6875370b7472
Quinten De Swaef <quintendeswaef@gmail.com>
2016-04-15 16:40:11 +0200

    fixed file copy

--- README.md
+++ README.md
@@ -1,6 +1,16 @@
 DockerFile for invoice ninja (https://www.invoiceninja.com/)
 
+#Running the container
+
+```
+docker run -d -p 127.0.0.1:9010:9000 --link pikachu-mysql:mysql -e DB_DATABASE=ninja -e DB_USERNAME=invoiceninja -e DB_HOST=mysql -e DB_PASSWORD=invoiceninjapwd qkyrie/invoiceninja:1.7
+```
+
+#Mounting Volumes
+
+```
 docker run -d -v "$PWD"/config:/var/www/app/config -v "$PWD"/public:/var/www/app/public -v "$PWD"/storage:/var/www/app/storage --name invoiceninja -p 127.0.0.1:9010:9000 --link pikachu-mysql:mysql -e DB_DATABASE=ninja -e DB_USERNAME=invoiceninja -e DB_HOST=mysql -e DB_PASSWORD=invoiceninjapwd qkyrie/invoiceninja:1.7
+```
 
 ##Installing
 
--- app-entrypoint.sh
+++ app-entrypoint.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
 set -e
 
-cp -avr /var/www/app/docker-backup-config/* /var/www/app/config/.
-cp -avr /var/www/app/docker-backup-public/* /var/www/app/public/.
-cp -avr /var/www/app/docker-backup-storage/* /var/www/app/storage/.
+cp -avr /var/www/app/docker-backup-config/. /var/www/app/config/.
+cp -avr /var/www/app/docker-backup-public/. /var/www/app/public/.
+cp -avr /var/www/app/docker-backup-storage/. /var/www/app/storage/.
 rm -rf /var/www/app/docker-backup-public/*
-rm -rf /var/wwww/app/docker-backup-config/*
-rm -rf /var/wwww/app/docker-backup-storage/*
+rm -rf /var/www/app/docker-backup-config/*
+rm -rf /var/www/app/docker-backup-storage/*
 
 exec "$@"
